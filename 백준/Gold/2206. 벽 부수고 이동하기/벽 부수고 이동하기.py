0d8189b10cd176e6160185a25cfba58bfdc826c4
박종민 <97735859+qooktree1@users.noreply.github.com>
2022-04-06 01:24:41 +0900

    [Gold IV] Title: 벽 부수고 이동하기, Time: 7304 ms, Memory: 190256 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -6,7 +6,7 @@ dy = [0, 0, -1, 1]
 
 def bfs(sx, sy, crash):
     q = deque([[sx, sy, crash]])
-    visited[sx][sy][crash] = 1  # 벽이 아닌 경우 : 0, 벽을 부시는 경우 : 1
+    visited[sx][sy][crash] = 1
     while q:
         x, y, crash = q.popleft()
         if x == ex and y == ey:
@@ -14,17 +14,18 @@ def bfs(sx, sy, crash):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny][crash] and lst[nx][ny] == 0:
-                q.append([nx, ny, crash])
+            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny][crash] and matrix[nx][ny] == 0:
                 visited[nx][ny][crash] = visited[x][y][crash] + 1
-            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny][crash] and lst[nx][ny] == 1 and crash == 0:
+                q.append([nx, ny, crash])
+            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny][crash] and matrix[nx][ny] == 1 and crash == 0:
+                visited[nx][ny][crash + 1] = visited[x][y][crash] + 1
                 q.append([nx, ny, crash + 1])
-                visited[nx][ny][crash+1] = visited[x][y][crash] + 1
-    return -1
+    return - 1
 
 
 N, M = map(int, input().split())
-lst = [list(map(int, input())) for _ in range(N)]
-ex, ey = N - 1, M - 1  # 도착점
-visited = [[[0] * 2 for _ in range(M)] for _ in range(N)]
-print(bfs(0, 0, 0))
+matrix = [list(map(int, input())) for _ in range(N)]
+visited = [[[0]*2 for _ in range(M)] for _ in range(N)]
+sx = sy = 0
+ex, ey = N-1, M-1
+print(bfs(sx, sy, 0))
