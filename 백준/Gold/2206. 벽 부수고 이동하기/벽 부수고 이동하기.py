c81fc536719e7d7b8018f00c8d1724025d204e27
박종민 <97735859+qooktree1@users.noreply.github.com>
2022-04-06 01:29:09 +0900

    [Gold IV] Title: 벽 부수고 이동하기, Time: 7108 ms, Memory: 190256 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -14,17 +14,23 @@ def bfs(sx, sy, crash):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
+            # 0으로 이동할때
             if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny][crash] and matrix[nx][ny] == 0:
                 visited[nx][ny][crash] = visited[x][y][crash] + 1
                 q.append([nx, ny, crash])
-            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny][crash] and matrix[nx][ny] == 1 and crash == 0:
+            # 벽으로 이동할때
+            elif 0 <= nx < N and 0 <= ny < M and not visited[nx][ny][crash] and matrix[nx][ny] == 1 and crash == 0:
+                # visited 배열에 crash 값을 증가시킴
                 visited[nx][ny][crash + 1] = visited[x][y][crash] + 1
                 q.append([nx, ny, crash + 1])
-    return - 1
+    return -1  # 이동 불가시 -1 출력
 
 
 N, M = map(int, input().split())
 matrix = [list(map(int, input())) for _ in range(N)]
+
+# matrix 한 칸 안에 벽을 부순 횟수를 기록할 [0, 0] 만들기
+# 초반에 임의의 변수를 사용했지만 BFS 로 접근할때 변수값이 변하지 않아 배열에 저장하는 방식을 씀
 visited = [[[0]*2 for _ in range(M)] for _ in range(N)]
 sx = sy = 0
 ex, ey = N-1, M-1
