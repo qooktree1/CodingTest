f3ebd7e7d861d6f85b701ac549e59af5e5fb5193
박종민 <97735859+qooktree1@users.noreply.github.com>
2022-05-01 13:57:59 +0900

    [Silver II] Title: 최소 힙, Time: 144 ms, Memory: 36764 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+N = int(input())
+heap = []
+for _ in range(N):
+    x = int(input())
+    if x == 0:
+        if not heap:
+            print(0)
+            continue
+        a = heapq.heappop(heap)
+        print(a)
+    else:
+        heapq.heappush(heap, x)
