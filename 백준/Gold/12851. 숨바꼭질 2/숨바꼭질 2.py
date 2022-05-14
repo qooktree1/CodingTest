ec5f19ff1c156d381cd26a6072dcc63586b7a9cd
박종민 <97735859+qooktree1@users.noreply.github.com>
2022-05-14 13:03:24 +0900

    [Gold V] Title: 숨바꼭질 2, Time: 620 ms, Memory: 36940 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -10,11 +10,14 @@ def bfs(X):
     while q:
         x = q.popleft()
 
+        # 도착시 방문횟수를 count & 다음 x 를 보기 위해 continue
         if x == K:
             ans += 1
             continue
 
         for n_x in [x+1, 2*x, x-1]:
+
+            # 주어진 범위 안에 존재 & 미방문 & 최단시간을 방문했던 흔적이 있으면
             if 0 <= n_x < 100001 and (not visited[n_x] or visited[n_x] == visited[x] + 1):
                 visited[n_x] = visited[x] + 1
                 q.append(n_x)
