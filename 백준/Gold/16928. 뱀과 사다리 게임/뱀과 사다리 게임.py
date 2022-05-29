154f03e51d73157f591c3d4832ce77d834e3f3eb
박종민 <97735859+qooktree1@users.noreply.github.com>
2022-05-29 13:37:08 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 104 ms, Memory: 32452 KB -BaekjoonHub

---
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -0,0 +1,36 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+from collections import deque
+
+def bfs(x):
+    q = deque()
+    q.append(x)
+    while q:
+        x = q.popleft()
+        for dice in range(1, 7):
+            n_x = x + dice
+            
+            # 100 칸을 넘어가서 이동 불가
+            if n_x <= 100:
+                if case[n_x]:
+                    n_x = case[n_x]
+                    
+                # 방문하지 않았다면 전의 방문한 곳에서 +1
+                if not visited[n_x]:
+                    q.append(n_x)
+                    visited[n_x] = visited[x] + 1
+
+
+N, M = map(int, input().split())
+case = [0 for _ in range(101)]
+visited = [0] * 101
+for _ in range(N):
+    x, y = map(int, input().split())
+    case[x] = y
+for _ in range(M):
+    u, v = map(int, input().split())
+    case[u] = v
+
+bfs(1)
+print(visited[100])
