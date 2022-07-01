c22b7012740af41cf9a98d2e61aab29a4e40b1c4
박종민 <97735859+qooktree1@users.noreply.github.com>
2022-07-01 09:58:10 +0900

    [Silver I] Title: 안전 영역, Time: 1500 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,45 +1,47 @@
+import sys
+input = sys.stdin.readline
+
+# 상하좌우
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
 def dfs(x, y):
     stack = [[x, y]]
+    visited[x][y] = 1
     while stack:
         x, y = stack.pop()
-        visited[x][y] = 1
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and area[nx][ny]:
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
+            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny] and area[nx][ny] > num:
                 stack.append([nx, ny])
+                visited[nx][ny] = 1
 
 
 N = int(input())
-area = [list(map(int, input().split())) for _ in range(N)]
-max_cnt = 0
+area = []
+max_num = 0
+min_num = 101
+ans = 0
+
+# 지역 그리기
+for _ in range(N):
+    area.append(list(map(int, input().split())))
+
+# 가장 (큰 숫자, 작은 숫자) 찾기
 for i in range(N):
     for j in range(N):
-        if max_cnt < area[i][j]:
-            max_cnt = area[i][j]
+        max_num = max(max_num, area[i][j])
+        min_num = min(min_num, area[i][j])
 
-
-cnt = 0
-max_ans = 0
-while cnt != max_cnt:
+for num in range(min_num-1, max_num+1):
     visited = [[0] * N for _ in range(N)]
+    cnt = 0
     for i in range(N):
         for j in range(N):
-            if area[i][j] <= cnt:
-                area[i][j] = 0
-    counts = 0
-    for p in range(N):
-        for q in range(N):
-            if not visited[p][q] and area[p][q]:
-                dfs(p, q)
-                counts += 1
-    if max_ans < counts:
-        max_ans = counts
-
-    cnt += 1
-
-print(max_ans)
+            if area[i][j] > num and not visited[i][j]:
+                cnt += 1
+                dfs(i, j)
+    ans = max(ans, cnt)
+print(ans)
