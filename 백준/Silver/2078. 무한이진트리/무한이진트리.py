a9459f104ddd9658d8f5d3ee1808c97a650fc49a
박종민 <97735859+qooktree1@users.noreply.github.com>
2022-07-16 16:48:36 +0900

    [Silver III] Title: 무한이진트리, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2078. 무한이진트리/무한이진트리.py
+++ 백준/Silver/2078. 무한이진트리/무한이진트리.py
@@ -2,13 +2,16 @@ import sys
 A, B = map(int, input().split())
 left = right = 0
 while 1:
+    # A: 1, B: 3일때
     if A == 1:
         right += B - 1
         break
+    # A: 3, B: 1일때
     elif B == 1:
         left += A - 1
         break
     if A < B:
+        # 11 4일때 오른쪽 이동을 2번 B는 3으로 바뀐다.
         right += B // A
         B = B % A
     else:
