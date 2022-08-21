1577e6c0d30008bbcee0358d726805c1f5a38e83
박종민 <97735859+qooktree1@users.noreply.github.com>
2022-08-22 00:34:17 +0900

    [Gold V] Title: 택배 배송, Time: 292 ms, Memory: 51732 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.py
+++ 백준/Gold/5972. 택배 배송/택배 배송.py
@@ -5,7 +5,7 @@ N, M = map(int, input().split())
 lst = [[] for _ in range(N+1)]
 
 for _ in range(M):
-    a, b, c = map(int ,input().split())
+    a, b, c = map(int, input().split())
     lst[a].append([b, c])
     lst[b].append([a, c])
 
@@ -18,11 +18,11 @@ q = [[0, 1]]
 while q:
     cost, x = heapq.heappop(q)
 
-    if visited[x] >= cost:
 
-        for n_x, n_c in lst[x]:
-            if visited[n_x] > cost + n_c:
-                visited[n_x] = cost + n_c
-                heapq.heappush(q, [cost + n_c, n_x])
+    for n_x, n_c in lst[x]:
+        # 현재 헛간을 거쳐서 다른 헛간으로 이동하는 거리가 더 짧은 경우
+        if visited[n_x] > cost + n_c:
+            visited[n_x] = cost + n_c
+            heapq.heappush(q, [cost + n_c, n_x])
 
 print(visited[N])
