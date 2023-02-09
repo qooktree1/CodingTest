78b0ff1aac22d804f66080ee8bcb22f57d050e25
박종민 <97735859+qooktree1@users.noreply.github.com>
2023-02-10 00:14:27 +0900

    [Gold IV] Title: 수열의 점수, Time: 276 ms, Memory: 31292 KB -BaekjoonHub

--- 백준/Gold/2036. 수열의 점수/수열의 점수.js
+++ 백준/Gold/2036. 수열의 점수/수열의 점수.js
@@ -1,49 +1,27 @@
-const fs = require("fs");
-const [n, ...numbers] = fs
-  .readFileSync("dev/stdin")
-  .toString()
-  .trim()
-  .split("\n")
-  .map(Number);
+const fs = require("fs")
+let [n, ...numbers] = fs.readFileSync("dev/stdin").toString().trim().split("\n").map(Number)
 
-let answer = 0n;
+const positiveNumbers = []
+const negativeNumbers = []
+let answer = 0n
 
-const plus = [];
-const minus = [];
-
-for (let i = 0; i < n; i++) {
-  if (numbers[i] > 1) {
-    plus.push(numbers[i]);
-  } else if (numbers[i] <= 0) {
-    minus.push(numbers[i]);
-  } else {
-    answer += 1n;
-  }
+for (let i=0; i<n; i++){
+    if (numbers[i] > 1) positiveNumbers.push(numbers[i])
+    else if (numbers[i] <= 0) negativeNumbers.push(numbers[i])
+    else answer += 1n  // 1은 어떤 수와 곱하면 손해 따라서 따로 빼준다
 }
 
-plus.sort((a, b) => b - a);
-minus.sort((a,b) => a - b);
+positiveNumbers.sort((a, b) => b-a)
+negativeNumbers.sort((a, b) => a-b)
 
-if (plus.length % 2 === 0) {
-  for (let i = 0; i < plus.length; i += 2) {
-    answer += BigInt(plus[i] * plus[i + 1]);
-  }
-} else {
-  for (let i = 0; i < plus.length - 1; i += 2) {
-    answer += BigInt(plus[i] * plus[i + 1]);
-  }
-  answer += BigInt(plus[plus.length - 1]);
+function checkNumbers(numbers, ssum){
+    if (numbers.length % 2 === 0) for (let i=0; i<numbers.length; i+=2) ssum += BigInt(numbers[i]*numbers[i+1])
+    else {
+        for (let i=0; i<numbers.length-1; i+=2) ssum += BigInt(numbers[i]*numbers[i+1])
+        ssum += BigInt(numbers[numbers.length-1])
+    }
+    return ssum
 }
-
-if (minus.length % 2 === 0) {
-  for (let i = 0; i < minus.length; i += 2) {
-    answer += BigInt(minus[i] * minus[i + 1]);
-  }
-} else {
-  for (let i = 0; i < minus.length - 1; i += 2) {
-    answer += BigInt(minus[i] * minus[i + 1]);
-  }
-  answer += BigInt(minus[minus.length - 1]);
-}
-
-console.log(answer.toString());
+answer += checkNumbers(positiveNumbers, 0n)
+answer += checkNumbers(negativeNumbers, 0n)
+console.log(answer.toString())
