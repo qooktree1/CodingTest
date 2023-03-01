8e14042310631397b0c76350904f4e9cb22a8439
박종민 <97735859+qooktree1@users.noreply.github.com>
2023-03-01 16:17:11 +0900

    [level 0] Title: 특이한 정렬, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -0,0 +1,7 @@
+function solution(numlist, n) {
+    return numlist.sort((a,b)=>{
+        if (Math.abs(n-a) === Math.abs(n-b)){
+            return b - a
+        } else return Math.abs(n-a) - Math.abs(n-b)
+    })
+}
