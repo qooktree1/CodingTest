e24daf10e777f7b027585768e7de28d03b8f7f94
박종민 <97735859+qooktree1@users.noreply.github.com>
2023-03-01 15:25:58 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.18 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -0,0 +1,10 @@
+function solution(a, b) {
+    let gcd  // 최대공약수
+    for (let i=1; i<=b; i++){
+        if (a % i === 0 && b % i === 0) gcd = i
+    }
+    b = b / gcd
+    while (b%2 === 0) b = b/2
+    while (b%5 === 0) b = b/5
+    return b === 1 ? 1 : 2
+}
