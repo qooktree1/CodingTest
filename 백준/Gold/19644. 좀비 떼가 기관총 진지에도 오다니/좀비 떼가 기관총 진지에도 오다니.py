6d78eda0bf81ee3f6b7f812a33e0cf8016759222
박종민 <97735859+qooktree1@users.noreply.github.com>
2023-04-09 09:02:39 +0900

    [Gold IV] Title: 좀비 떼가 기관총 진지에도 오다니, Time: 3036 ms, Memory: 267952 KB -BaekjoonHub

--- 백준/Gold/19644. 좀비 떼가 기관총 진지에도 오다니/좀비 떼가 기관총 진지에도 오다니.py
+++ 백준/Gold/19644. 좀비 떼가 기관총 진지에도 오다니/좀비 떼가 기관총 진지에도 오다니.py
@@ -1,36 +1,25 @@
 import sys
 input = sys.stdin.readline
-L = int(input())
-Ml, Mk = map(int, input().split())
+
+N = int(input())
+len, damage = map(int, input().split())
 C = int(input())
-flag = True
-map_lst = [0] * L
-bomber_lst = [False] * L
-start, end = -Ml + 1, 0
-Bomb, shootDam, fullDam = Ml - 1, 0, Mk * Ml
+arr = [0]
+for _ in range(N):
+    arr.append(int(input()))
 
-for i in range(L):
-    map_lst[end] = int(input().rstrip())
+psum = [0] * (N + 1)
 
-    shootDam = fullDam - Bomb * Mk
+for i in range(1, N+1):
+    now = psum[i-1] - psum[max(0, i-len)]
+    if arr[i] <= now + damage:
+        psum[i] = psum[i-1] + damage
+    else:
+        if C:
+            C -= 1
+            psum[i] = psum[i-1]
+        else:
+            print("NO")
+            sys.exit()
 
-    if shootDam < map_lst[end]:
-        C -= 1
-        if C < 0:
-            flag = False
-            break
-        Bomb += 1
-        bomber_lst[end] = True
-
-    if start < 0 or bomber_lst[start]:
-        Bomb -= 1
-        start += 1
-        end += 1
-
-    if end == L:
-        break
-
-if flag == True:
-    print("YES")
-else:
-    print("NO")
+print("YES")
