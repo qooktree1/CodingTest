d3933e1a6fc3d588fa3faf019d182064bf7f912f
박종민 <97735859+qooktree1@users.noreply.github.com>
2023-04-15 21:35:50 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,16 @@
+// 문제: 멀리 뛰기
+// 시간:
+// 알고리즘: DP
+
+const solution = n => {
+    const dp = Array(n+1).fill(0)
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 2
+    
+    for (let i=3; i<=n; i++){
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567
+    }
+    
+    return dp[n]
+}
