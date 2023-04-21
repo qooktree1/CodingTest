a19abfb12122cdd508211187476de8712f9b5e9f
박종민 <97735859+qooktree1@users.noreply.github.com>
2023-04-21 23:35:42 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 3.49 ms, Memory: 42.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,7 @@
+function solution(arr) {
+    return arr.map(x => {
+        if (x >= 50 && x % 2 === 0) return x / 2
+        else if (x < 50 && x % 2) return x * 2
+        else return x
+    })
+}
