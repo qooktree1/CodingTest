9947a8d78ee191e0386f6f22d30422ffff1fb4ff
박종민 <97735859+qooktree1@users.noreply.github.com>
2023-04-22 01:15:42 +0900

    [unrated] Title: 문자열 여러 번 뒤집기, Time: 9.20 ms, Memory: 35 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181913. 문자열 여러 번 뒤집기/문자열 여러 번 뒤집기.js
@@ -0,0 +1,6 @@
+function solution(my_string, queries) {
+    for (const query of queries){
+        my_string = my_string.slice(0, query[0]) + my_string.slice(query[0], query[1]+1).split("").reverse().join("") + my_string.slice(query[1]+1)
+    }
+    return my_string
+}
