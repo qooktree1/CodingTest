9c05f68417c18d48a3a133bddda39c73111a83dc
박종민 <97735859+qooktree1@users.noreply.github.com>
2023-05-08 17:05:03 +0900

    [unrated] Title: 호텔 대실, Time: 7.07 ms, Memory: 37.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.js
@@ -0,0 +1,17 @@
+const solution = bookTime => {
+    let ans = 0
+    const timeMap = new Map()
+    const check = []
+    for (const time of bookTime){
+        const start = Number(time[0].slice(0, 2) * 60) + Number(time[0].slice(3))
+        const end = Number(time[1].slice(0, 2) * 60) + Number(time[1].slice(3)) + 10
+        
+        for (let i=start; i<end; i++){
+            if (timeMap.get(i) === undefined) timeMap.set(i, 1)
+            else timeMap.set(i, timeMap.get(i) + 1)
+        }
+    }
+    
+    // timeMap에서 가장 큰 value 찾기
+    return [...timeMap.entries()].reduce((a,b) => a[1] > b[1] ? a : b)[1]
+}
