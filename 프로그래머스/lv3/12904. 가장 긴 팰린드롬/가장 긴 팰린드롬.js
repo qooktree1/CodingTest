65e6e6f6b63aac2e837293c2418cb3e20b5bd77a
박종민 <97735859+qooktree1@users.noreply.github.com>
2023-05-18 03:47:17 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 30.51 ms, Memory: 36.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.js
@@ -0,0 +1,34 @@
+// 문제: 가장 긴 팰린드롬
+// 시간: 36분
+// 알고리즘: slicing, 홀수 짝수 나누기
+
+
+const solution = s => {
+    let ans = 0
+    
+    for (let i=0; i<s.length; i++){
+        const tmp = s[i]
+        let index = 1
+        
+        // 홀수
+        while (i-index>=0 && i+index < s.length){
+            if (s[i-index] === s[i+index]) index++
+            else break
+        }
+        ans = Math.max(ans, (index-1) * 2 + 1)
+        
+        let index2 = 1
+        // 짝수
+        if (i+1 < s.length && tmp === s[i+1]){
+            while (i-index2 >= 0 && i+index2+1 < s.length){
+                if (s[i+index2+1] === s[i-index2]) index2++
+                else break
+            }
+            ans = Math.max(ans, (index2-1) *2 + 2)
+        }
+        
+        
+    }
+    
+    return ans
+}
