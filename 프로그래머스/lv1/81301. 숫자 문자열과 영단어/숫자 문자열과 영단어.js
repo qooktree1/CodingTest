319a6bbc759f45f9593a73c387c80352c4eadc40
박종민 <97735859+qooktree1@users.noreply.github.com>
2023-09-21 19:10:03 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,31 +1,22 @@
-function solution(s) {
-    let result = ""
-    const numbers = {
-        "zero": "0",
-        "one": "1",
-        "two": "2",
-        "three": "3",
-        "four": "4",
-        "five": "5",
-        "six": "6",
-        "seven": "7",
-        "eight": "8",
-        "nine": "9"
+const solution = s => {
+    let ans = ""
+    const numberDict = {
+        "zero": "0", "one": "1", "two": "2", "three": "3", "four": "4", "five": "5",
+        "six": "6", "seven": "7", "eight": "8", "nine": "9"
     }
-    const newS = s.split("")
     let tmp = ""
-    for (let i=0; i<newS.length; ++i){
-        
-        if (!isNaN(newS[i])){
-            result += newS[i]
-        } else {
-            tmp += newS[i]
-            if (numbers[tmp] !== undefined) {
-                result += numbers[tmp]
+    for (let i=0; i<s.length; i++){
+        if (numberDict[tmp]){
+                ans += numberDict[tmp]
                 tmp = ""
             }
+        if (!isNaN(s[i])){
+            ans += s[i]
+            tmp = ""
         }
-        
+        else if (isNaN(s[i])) tmp += s[i]
     }
-    return Number(result)
+    if (tmp.length > 0) ans += numberDict[tmp]
+    
+    return Number(ans)
 }
