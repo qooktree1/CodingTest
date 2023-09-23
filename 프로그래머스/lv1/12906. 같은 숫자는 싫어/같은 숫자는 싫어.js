9aab9f7f2b5eaf336848b068c636bf2bb32ea1e2
박종민 <97735859+qooktree1@users.noreply.github.com>
2023-09-23 20:36:38 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 13.61 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,12 +1,12 @@
-function solution(arr)
-{
-    const answer = [arr[0]]
-    let tmp = arr[0]
-    for (let i=1; i<arr.length; ++i){
-        if (tmp !== arr[i]){
-            answer.push(arr[i])
-            tmp = arr[i]
+const solution = arr => {
+    const ans = []
+    ans.push(arr[0])
+    let compare = arr[0]
+    for (let i=1; i<arr.length; i++){
+        if (compare !== arr[i]){
+            ans.push(arr[i])
+            compare = arr[i]
         }
     }
-    return answer
+    return ans
 }
