382f05899de5ac7e3955adc220e37e52158cd63f
박종민 <97735859+qooktree1@users.noreply.github.com>
2023-09-28 23:00:28 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,7 +1,7 @@
-function solution(lottos, win_nums) {
-    const certainty = lottos.filter(lotto => win_nums.includes(lotto)).length  // 확실히 맞는 번호 개수
-    const zeros = lottos.filter(lotto => lotto === 0).length  // 0의 개수
-    const min = 7 - certainty >= 6 ? 6 : 7 - certainty  // 일치하는 번호가 1개 이하일 경우 6등
-    const max = 7 - (certainty + zeros) >= 6 ? 6 : 7 - (certainty + zeros)
+const solution = (lottos, winNums) => {
+    const actualWin = lottos.filter(lotto => winNums.includes(lotto)).length  // 확실히 맞는 번호 개수
+    const zeroCnt = lottos.filter(lotto => lotto === 0).length  // 0의 개수
+    const min = actualWin > 1 ? 7 - actualWin : 6
+    const max = actualWin + zeroCnt > 1 ? 7 - (actualWin + zeroCnt) : 6
     return [max, min]
 }
