0051dbd947b25b25f49f966cc2c8c6a504f96691
박종민 <97735859+qooktree1@users.noreply.github.com>
2023-09-29 11:35:01 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.49 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -1,60 +1,44 @@
-function solution(numbers, hand) {
-    const direction={
-        left:[1,4,7],
-        right:[3,6,9],
-    };
-    const numberPosition={
-        1:[0,0],
-        2:[0,1],
-        3:[0,2],
-        4:[1,0],
-        5:[1,1],
-        6:[1,2],
-        7:[2,0],
-        8:[2,1],
-        9:[2,2],
-        '*':[3,0],
-        0:[3,1],
-        '#':[3,2],
-    }
-    let left=numberPosition['*'];
-    let right=numberPosition['#'];
-    let answer='';
-    
-    numbers.forEach((number)=>{
-        if(direction.left.includes(number)){
-            answer+='L';
-            left=numberPosition[number];
-        }
-        else if(direction.right.includes(number)){
-            answer +='R';
-            right=numberPosition[number];
-        }
-        else{
-            let[x,y]=numberPosition[number];
-            let[lx,ly]=left;
-            let[rx,ry]=right;
-            let leftDistance=Math.abs(x-lx)+Math.abs(y-ly);
-            let rightDistance=Math.abs(x-rx)+Math.abs(y-ry);
-            
-            if(leftDistance>rightDistance){
-                right=[x,y];
-                answer+='R';
-            }
-            else if (rightDistance > leftDistance) {
-        left = [x, y];
-        answer += 'L';
-      } else {
-        if (hand === 'right') {
-          right = [x, y];
-          answer += 'R';
-        } else {
-          left = [x, y];
-          answer += 'L';
-        }
-      }
-    }
-  });
+const distCalc = (left, right, pos) => {
+    const leftDist = Math.abs(pos[0] - left[0]) + Math.abs(pos[1] - left[1])
+    const rightDist = Math.abs(pos[0] - right[0]) + Math.abs(pos[1] - right[1])
+    if (rightDist > leftDist) return "left"
+    else if (rightDist < leftDist) return "right"
+    else return 0
+}
 
-  return answer;
+
+const solution = (numbers, hand) => {
+    const keypad = {
+        "1": [0, 0], "2": [0, 1], "3": [0, 2],
+        "4": [1, 0], "5": [1, 1], "6": [1, 2],
+        "7": [2, 0], "8": [2, 1], "9": [2, 2],
+        "*": [3, 0], "0": [3, 1], "#": [3, 2]
+    }
+    let [leftHand, rightHand] = [keypad["*"], keypad["#"]]
+    let ans = ""
+    numbers.forEach(number => {
+        if (number === 1 || number === 4 || number === 7){
+            ans += "L"
+            leftHand = keypad[String(number)]
+        } else if (number === 3 || number === 6 || number === 9){
+            ans += "R"
+            rightHand = keypad[String(number)]
+        } else {
+            const result = distCalc(leftHand, rightHand, keypad[String(number)])
+            if (result === "left"){
+                ans += "L"
+                leftHand = keypad[String(number)]
+            } else if (result === "right"){
+                ans += "R"
+                rightHand = keypad[String(number)]
+            } else if (result === 0 && hand === "right") {
+                ans += "R"
+                rightHand = keypad[String(number)]
+            } else if (result === 0 && hand === "left") {
+                ans += "L"
+                leftHand = keypad[String(number)]
+            }
+        }
+    })
+    return ans
 }
