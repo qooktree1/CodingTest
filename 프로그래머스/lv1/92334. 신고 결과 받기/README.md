20d99f40e829f013fc643bfe4b8156c01a490c8c
박종민 <97735859+qooktree1@users.noreply.github.com>
2023-10-04 23:44:55 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/README.md
+++ 프로그래머스/lv1/92334. 신고 결과 받기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신고 결과 받기 - 92334 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92334) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.11 ms
+메모리: 33.5 MB, 시간: 0.13 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+정확성: 100.0<br/>합계: 100.0 / 100.0
 
 ### 문제 설명
 
--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,29 +1,37 @@
-function solution(id_list, report, k) {
-    const fixedReport = [...new Set(report)] // 중복 제
-    const reportedId = []
-    const reported = Array(id_list.length).fill(0) // 신고 count 배열
-    const mail = Array(id_list.length).fill(0) // 결과 메일 배열
+const solution = (idList, reports, k) => {
+    const reportDict = {}
+    const reportConnection = {}
+    const suspendedId = []
+    const ans = []
     
-    for (let r of fixedReport){
-        const [a, b] = r.split(" ") // a: 신고한 ID, 신고받은 ID
-        reported[id_list.indexOf(b)] += 1 // 신고받은 ID에 1씩 증가
-        
-        // 신고수가 k개 이상인 경우
-        if (reported[id_list.indexOf(b)] >= k){
-            reportedId.push(id_list[id_list.indexOf(b)])
+    for (const id of idList){
+        reportDict[id] = 0
+        reportConnection[id] = []
+    }
+    
+    for (const report of reports){
+        const [from, to] = report.split(" ")
+        if (!reportConnection[from].includes(to)){
+            reportConnection[from].push(to)
+            reportDict[to] += 1  // 신고받은 횟수 저장
         }
     }
     
-    fixedReport.map((r) => {
-        const [a, b] = r.split(" ")
-        
-        // 유저가 신고한 ID(b)와 정지된 ID가 같을때 찾기
-        if (reportedId.indexOf(b) >= 0){
-            mail[id_list.indexOf(a)] += 1
+    for (const report in reportDict){
+        if (k <= reportDict[report]){
+            suspendedId.push(report)
         }
-    })
+    }
     
-    return mail
-    
-    
+    for (const id of idList){
+        let cnt = 0
+        for (const reportId of reportConnection[id]){
+            if (reportDict[reportId] >= k){
+                cnt++
+            }
+        }
+        ans.push(cnt)
+
+    }
+    return ans
 }
