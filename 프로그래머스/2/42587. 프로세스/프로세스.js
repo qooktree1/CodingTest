e7074c65a10bac3e33a767fb539c909a7ffcaa66
박종민 <97735859+qooktree1@users.noreply.github.com>
2023-11-01 23:51:07 +0900

    [level 2] Title: 프로세스, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,12 @@
+const solution = (priorities, location) => {
+    const waitingQ = priorities.map((priority, index) => [priority, index])
+    const N = priorities.length
+    // while (true) {
+    //     priorities.pop()
+    // }
+    console.log(waitingQ)
+    
+}
+
+// A, B, C, D
+// C D A B
