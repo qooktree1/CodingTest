1b5442d9b56a5c59eb931d08231239b64d818ec0
박종민 <97735859+qooktree1@users.noreply.github.com>
2023-11-06 00:08:55 +0900

    [level 2] Title: 타겟 넘버, Time: 1.15 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,19 +1,18 @@
 let answer = 0
 
-const dfs = (n, ssum, target, N, numbers) => {
+const dfs = (n, ssum, target, numbers) => {
     // 종료 조건
-    if (n === N) {
+    if (n === numbers.length) {
         if (ssum === target) answer++
         return
     }
     
-    dfs(n + 1, ssum + numbers[n], target, N, numbers)
-    dfs(n + 1, ssum - numbers[n], target, N, numbers)
+    dfs(n + 1, ssum + numbers[n], target, numbers)
+    dfs(n + 1, ssum - numbers[n], target, numbers)
 }
 
 
 const solution = (numbers, target) => {
-    
-    dfs(0, 0, target, numbers.length, numbers)
+    dfs(0, 0, target, numbers)
     return answer
 }
