e1626e26ea9d169bfeebec040a036ca024e57776
박종민 <97735859+qooktree1@users.noreply.github.com>
2023-12-23 23:44:45 +0900

    [level 2] Title: 방문 길이, Time: 0.63 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,24 +1,16 @@
 const solution = dirs => {
-    const dirTable = {
-        "U": [-1, 0],
-        "D": [1, 0],
-        "L": [0, -1],
-        "R": [0, 1]
-    }
-    const visitedSet = new Set()
-    let x = 0, y = 0
+    const dirDict = { 'U': [-1, 0], 'D': [1, 0], 'L': [0, -1], 'R': [0, 1]}
+    const visited = new Set()
+    let [x, y] = [0, 0]
     
-    for (const dir of dirs) {
-        const nextMove = dirTable[dir]
-        let nx = x + nextMove[0]
-        let ny = y + nextMove[1]
-        
+    for (let i=0; i<dirs.length; i++) {
+        let [nx, ny] = [x + dirDict[dirs[i]][0], y + dirDict[dirs[i]][1]]
         if (-5 <= nx && nx <= 5 && -5 <= ny && ny <= 5) {
-            visitedSet.add([x, y, nx, ny].join(""))
-            visitedSet.add([nx, ny, x, y].join(""))
+            visited.add(String(x) + String(y) + String(nx) + String(ny))
+            visited.add(String(nx) + String(ny) + String(x) + String(y))
             x = nx
             y = ny
         }
     }
-    return visitedSet.size / 2
+    return visited.size / 2
 }
