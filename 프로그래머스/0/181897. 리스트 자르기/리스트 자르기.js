b5e9de56fedd72c41cbaa130127603dc8c197530
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-03-11 10:05:40 +0900

    [level 0] Title: 리스트 자르기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -3,5 +3,5 @@ const solution = (n, slicer, numbers) => {
     if (n === 1) return numbers.slice(0, b + 1)
     if (n === 2) return numbers.slice(a)
     if (n === 3) return numbers.slice(a, b + 1)
-    if (n === 4) return numbers.slice(a, b + 1).filter((_, idx) => !(idx %= c))
+    if (n === 4) return numbers.slice(a, b + 1).filter((_, idx) => !(idx % c))
 }
