91fc3c150c492669c98bb65844e4bb2deb855ae1
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-03-12 16:46:54 +0900

    [level 0] Title: 짝수의 합, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -1,7 +1,1 @@
-const solution = n => {
-    let ssum = 0
-    for (let i=2; i<=n; i++) {
-        if (i % 2 === 0) ssum += i
-    }
-    return ssum
-}
+const solution = n => Math.floor(n/2) * (Math.floor(n/2) + 1)
