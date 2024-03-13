1903437704e9e38696ae7917440b2b406f8d3c09
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-03-13 14:02:41 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,6 @@
+const solution = dot => {
+    if (dot[0] > 0 && dot[1] > 0) return 1
+    if (dot[0] > 0 && dot[1] < 0) return 4
+    if (dot[0] < 0 && dot[1] > 0) return 2
+    if (dot[0] < 0 && dot[1] < 0) return 3
+}
