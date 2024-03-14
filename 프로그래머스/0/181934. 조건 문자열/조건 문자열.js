dfbeb9f1b59f52ace8c21044edaf1d0b14e562f0
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-03-15 01:01:04 +0900

    [level 0] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,9 @@
+const solution = (ineq, eq, n, m) => {
+    if (ineq === '>') {
+        if (eq === '=') return n >= m ? 1 : 0
+        return n > m ? 1 : 0
+    } else {
+        if (eq === '=') return n <= m ? 1 : 0
+        return n < m ? 1 : 0
+    }
+}
