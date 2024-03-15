1975701538ee4b6266f62b7fdc716bc39cdafbd8
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-03-15 10:22:04 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.04 ms, Memory: 38.3 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.js
@@ -3,9 +3,6 @@ const solution = arr => {
     const rowLen = arr[0].length
     
     if (colLen === rowLen) return arr
-    if (colLen > rowLen) {
-        return arr.map(a => [...a, ...new Array(Math.max(colLen-rowLen, 0)).fill(0)])
-    } else {
-        return [...arr, ...Array.from(new Array(rowLen - colLen), () => Array(rowLen).fill(0))]
-    }
+    if (colLen > rowLen) return arr.map(a => [...a, ...new Array(Math.max(colLen-rowLen, 0)).fill(0)])
+    return [...arr, ...Array.from(new Array(rowLen - colLen), () => Array(rowLen).fill(0))]
 }
