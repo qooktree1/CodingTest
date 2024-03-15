478e1b13bfc58e68f8e3cc58df59283b2f8b5f0e
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-03-16 00:30:08 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 3.12 ms, Memory: 36.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -0,0 +1,10 @@
+const solution = (arr, queries) => {
+    return queries.map(query => {
+        const [s, e, k] = query
+        let minNum = 1000001
+        for (let i=s; i<=e; i++) {
+            if (arr[i] > k) minNum = Math.min(minNum, arr[i])
+        }
+        return minNum === 1000001 ? -1 : minNum
+    })
+}
