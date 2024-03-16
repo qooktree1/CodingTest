caad261967feee74df7486f9d1a10f0a2564c2d5
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-03-16 17:24:54 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.33 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,9 +1,9 @@
 const solution = numbers => {
     const answer = new Set()
-    for (let i=0; i<numbers.length-1; i++) {
-        for (let j=i+1; j<numbers.length; j++) {
+    for (let i=0; i<numbers.length - 1; i++) {
+        for (let j = i + 1; j<numbers.length; j++) {
             answer.add(numbers[i] + numbers[j])
         }
     }
-    return [...answer].sort((a,b) => a-b)
+    return [...answer].sort((a,b) => a - b)
 }
