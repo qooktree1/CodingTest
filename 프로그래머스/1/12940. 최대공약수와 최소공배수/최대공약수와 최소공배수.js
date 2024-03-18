d173897550cbd6121df23abbd2c91e7032795b9b
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-03-18 11:27:53 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 36.99 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,8 @@
+const solution = (n, m) => {
+    let gcd = 1, lcm
+    for (let i=1; i<=n*m; i++) {
+        if (n % i === 0 && m % i === 0) gcd = i
+        if (!lcm && i % n === 0 && i % m === 0) lcm = i
+    }
+    return [gcd, lcm]
+}
