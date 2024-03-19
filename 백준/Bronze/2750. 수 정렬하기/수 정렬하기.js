5e75bd5f4ddaeabdd575bfdbc10e9686ee562c0a
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-03-19 22:20:04 +0900

    [Bronze II] Title: 수 정렬하기, Time: 208 ms, Memory: 11208 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -0,0 +1,11 @@
+let input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+let arr = input.slice(1, input.length);
+arr.sort((a, b) => a - b);
+
+for (let i = 0; i < arr.length; i++) console.log(arr[i]);
