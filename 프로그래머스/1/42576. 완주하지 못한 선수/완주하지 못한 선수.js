5292c7cf3721a86ed5fe3cf448130d269bb1d474
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-03-29 10:31:12 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 103.33 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,11 +1,13 @@
 const solution = (participants, completion) => {
-    const participantMap = {}
-    for (const participant of participants) {
-        participantMap[participant] = participantMap[participant] ? participantMap[participant] + 1 : 1
-    }
-    
-    for (const test of completion) {
-        participantMap[test]--
-    }
-    return Object.entries(participantMap).filter(item => item[1] === 1)[0][0]
-}
+  const participantMap = {};
+  for (const participant of participants) {
+    participantMap[participant] = participantMap[participant]
+      ? participantMap[participant] + 1
+      : 1;
+  }
+
+  for (const test of completion) {
+    participantMap[test]--;
+  }
+  return Object.entries(participantMap).filter((item) => item[1] === 1)[0][0];
+};
