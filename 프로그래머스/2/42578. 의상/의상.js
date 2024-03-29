ebf36d1a414d0a4b4f24559fae7d1fe6c1b44c58
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-03-29 13:23:45 +0900

    [level 2] Title: 의상, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,17 +1,7 @@
-// 객체 만들고 value로 이루어진 배열로 바꾸기
-const createClothObject = (clothes) => {
-    const clothObject = {}
-    for (const cloth of clothes) {
-        if (cloth[1] in clothObject) clothObject[cloth[1]] += 1
-        else clothObject[cloth[1]] = 1
-    }
-    return [...Object.values(clothObject)]
-}
-
 const solution = clothes => {
-    const clothArr = createClothObject(clothes)
-    let answer = 0
-    console.log(clothArr)
-    answer += clothArr.reduce((a,b) => a * (b + 1), 1)  // 입지 않는 경우까지 포함해서 곱하기
-    return answer - 1  // 아무것도 입지 않는 경우 빼기
+    const clothMap = {}
+    for (const cloth of clothes) {
+        clothMap[cloth[1]] = (clothMap[cloth[1]] || 0) + 1
+    }
+    return Object.values(clothMap).reduce((acc, cur) => acc * (cur + 1), 1) - 1
 }
