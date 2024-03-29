5171fcac48c5cf576ba1e4541a03cbbea5a78292
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-03-29 10:29:10 +0900

    [level 1] Title: 폰켓몬, Time: 0.24 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,6 +1,1 @@
-const solution = nums => {
-    const numSet = new Set()
-    for (const num of nums) numSet.add(num)
-    
-    return Math.min(nums.length / 2, numSet.size)
-}
+const solution = nums => Math.min(nums.length / 2, new Set(nums).size)
