80efcf1712ac75de6da7ada1a7af3b668b17c1d0
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-03-29 10:14:04 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,5 +1,1 @@
-const solution = s => {
-    const lowerCase = s.split("").filter(c => c === c.toLowerCase()).sort((a,b) => b.localeCompare(a)).join("")
-    const upperCase = s.split("").filter(c => c === c.toUpperCase()).sort((a,b) => b.localeCompare(a)).join("")
-    return lowerCase + upperCase
-}
+const solution = s => s.split("").sort((a,b) => b.charCodeAt() - a.charCodeAt()).join("")
