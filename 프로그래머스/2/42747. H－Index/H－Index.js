ff1776cfbde765520ca90334b408953adb74065a
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-04-01 12:32:09 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,25 +1,7 @@
 const solution = citations => {
-    citations.sort((a, b) => b-a)
-    let answer = citations.length
-    
-    while (answer > 0) {
-        const isLarge = citations.slice(0, answer)
-        const isSmall = citations.slice(answer + 1)
-        let flag = true
-        for (let i=0; i<isLarge.length; i++) {
-            if (answer > isLarge[i]) {
-                flag = false
-                break
-            }
-        }
-        for (let i=0; i<isSmall.length; i++) {
-            if (answer < isSmall[i]) {
-                flag = false
-                break
-            }
-        }
-        if (flag) return answer
-        answer--
+    citations.sort((a,b) => b - a)
+    for (let i=0; i<citations.length; i++) {
+        if (citations[i] <= i) return i
     }
-    return answer
+    return citations.length
 }
