0dd42b52541e41f3e8353307a6eaeb3e6ffae2b5
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-04-03 10:20:42 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,4 +1,1 @@
-const solution = (array, commands) => commands.map(([i, j, k]) => {
-        const tmpArr = array.slice()
-        return tmpArr.slice(i - 1, j).sort((a,b) => a - b)[k - 1]
-    })
+const solution = (array, commands) => commands.map(([i, j, k]) => array.slice(i - 1, j).sort((a,b) => a - b)[k - 1])
