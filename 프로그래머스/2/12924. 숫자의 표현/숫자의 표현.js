85a1c71cd64af20ccba7c63abacbee963300673a
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-04-05 11:43:13 +0900

    [level 2] Title: 숫자의 표현, Time: 1.73 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,14 +1,14 @@
-const solution = n => {
-    let s = 0, e = 0
-    let ssum = 0
-    let answer = 0
-    while (s <= e && e <= n) {
-        if (e === n && ssum < n) return answer
-        if (ssum < n) ssum += ++e
-        else if (ssum >= n) {
-            if (ssum === n) answer++
-            ssum -= ++s
-        }
+const solution = (n) => {
+  let [s, e, numberSum, answer] = [1, 1, 1, 0];
+
+  while (s <= e && e <= n) {
+    if (e === n && numberSum < n) return answer;
+
+    if (numberSum < n) numberSum += ++e;
+    else if (numberSum >= n) {
+      if (numberSum === n) answer++;
+      numberSum -= s++;
     }
-    return answer
-}
+  }
+  return answer;
+};
