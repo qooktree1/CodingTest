d545ca066dbf58736d9ddfab1ac3528ddf088690
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-04-06 16:56:41 +0900

    [level 2] Title: 카펫, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,9 +1,16 @@
-// yellow(카펫의 가로 세로 크기를 구하는 문제)
-
+// total = yellow + brown = width * height
+// yellow = (width - 2) * (height - 2)
+// yellow = width * height - 2 * (width + height) + 4
 const solution = (brown, yellow) => {
-    
-    for (let height = 1; height <= yellow; height += 1){
-        const width = Number(yellow / height)
-        if (brown === width * 2 + height * 2 + 4 && width >= height) return [width + 2, height + 2]
-    }
+   const total = brown + yellow
+   
+   for (let width=total-1; width > 0; width--) {
+       if (total % width) continue
+       
+       const height = total / width
+       const y = (width - 2) * (height - 2)
+       const b = total - yellow
+       
+       if (yellow == y && b === brown) return [width, height]
+   }
 }
