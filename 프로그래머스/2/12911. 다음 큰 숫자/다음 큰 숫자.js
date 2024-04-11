3489a0f887f583c8476eadb8b6fdaf565750085c
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-04-11 17:11:42 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.08 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,8 @@
+const countOnes = num => [...num.toString(2)].filter(c => c === '1').length
+
+const solution = n => {
+    const oneCnt = countOnes(n)
+    for (let i = n + 1; i<=1000000; i++) {
+        if (oneCnt === countOnes(i)) return i
+    }
+}
