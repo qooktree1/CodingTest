abe45325e8839762930c38d45542acf8b3588ee8
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-04-12 11:35:03 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 4.44 ms, Memory: 36.6 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/README.md
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수열과 구간 쿼리 2 - 181923 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923) 
 
 ### 성능 요약
 
-메모리: 36.5 MB, 시간: 3.12 ms
+메모리: 36.6 MB, 시간: 4.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 16일 00:30:04
+2024년 04월 12일 11:35:02
 
 ### 문제 설명
 
--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -1,10 +1,9 @@
-const solution = (arr, queries) => {
-    return queries.map(query => {
-        const [s, e, k] = query
-        let minNum = 1000001
-        for (let i=s; i<=e; i++) {
-            if (arr[i] > k) minNum = Math.min(minNum, arr[i])
-        }
-        return minNum === 1000001 ? -1 : minNum
-    })
-}
+const solution = (arr, queries) =>
+  queries.map((query) => {
+    const [s, e, k] = query;
+    let minNum = 1000001;
+    for (let i = s; i <= e; i++) {
+      if (arr[i] > k) minNum = Math.min(minNum, arr[i]);
+    }
+    return minNum === 1000001 ? -1 : minNum;
+  });
