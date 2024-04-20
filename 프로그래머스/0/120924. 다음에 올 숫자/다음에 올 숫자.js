f35e60387edd1d74339f23d8b3805e9cac127c99
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-04-20 16:14:39 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -0,0 +1,4 @@
+const solution = common => {
+    if (common[2] - common[1] === common[1] - common[0]) return common[common.length - 1] + common[2] - common[1]
+    return common[common.length - 1] * (common[1] / common[0])
+}
