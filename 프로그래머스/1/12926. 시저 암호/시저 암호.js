6c8f83a6762e293e70062a2fefc2bcfa241a5f17
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-04-22 10:53:51 +0900

    [level 1] Title: 시저 암호, Time: 7.82 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -5,14 +5,8 @@ const solution = (s, n) => {
     for (let i=0; i<arr.length; i++) {
         if (arr[i] === " ") continue
         const cNumber = arr[i].charCodeAt() + n
-        
-        if (isLowerCase(arr[i])) {
-            if (cNumber > 122) arr[i] = String.fromCharCode(cNumber - 26)
-            else arr[i] = String.fromCharCode(cNumber)
-        } else {
-            if (cNumber > 90) arr[i] = String.fromCharCode(cNumber - 26)
-            else arr[i] = String.fromCharCode(cNumber)
-        }
+        if ((cNumber > 90 && !isLowerCase(arr[i])) || (cNumber > 122 && isLowerCase(arr[i])))arr[i] = String.fromCharCode(cNumber - 26)
+        else arr[i] = String.fromCharCode(cNumber)
     }
     return arr.join("")
 }
