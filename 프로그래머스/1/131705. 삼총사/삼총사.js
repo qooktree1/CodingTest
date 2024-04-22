46907209dd651d48e8fa3f7d3174ef6e928c245c
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-04-22 10:45:24 +0900

    [level 1] Title: 삼총사, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.js
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -1,11 +1,11 @@
-const solution = number => {
-    let answer = 0
-    for (let i=0; i<number.length - 2; i++) {
-        for (let j=i+1; j<number.length - 1; j++) {
-            for (let k=j+1;k<number.length; k++) {
-                if (number[i] + number[j] + number[k] === 0) answer++
-            }
-        }
+const solution = (number) => {
+  let answer = 0;
+  for (let i = 0; i < number.length - 2; i++) {
+    for (let j = i + 1; j < number.length - 1; j++) {
+      for (let k = j + 1; k < number.length; k++) {
+        if (number[i] + number[j] + number[k] === 0) answer++;
+      }
     }
-    return answer
-}
+  }
+  return answer;
+};
