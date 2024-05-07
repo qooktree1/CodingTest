36c31b38664b66ffd0e2376d3fa269d8ebd140bb
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-05-07 17:53:57 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 4.49 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -2,8 +2,7 @@ const solution = s => {
     const cDict = {}
     const answer = []    
     for (let i=0; i<s.length; i++) {
-        if (cDict[s[i]] === undefined) answer.push(-1)
-        else answer.push(i - cDict[s[i]])
+        answer.push(cDict[s[i]] === undefined ? -1 : i - cDict[s[i]])
         cDict[s[i]] = i
     }
     return answer
