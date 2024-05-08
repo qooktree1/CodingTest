0a04810c3a6c9a21ba22032aff2278a242c29e9d
박종민 <97735859+qooktree1@users.noreply.github.com>
2024-05-08 21:18:34 +0900

    Time: 56 ms (47.75%), Space: 51.3 MB (27.22%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -3,7 +3,6 @@ const diagonalSum = mat => {
     let answer = 0
     for (let i=0; i<n; i++) {
         answer += mat[i][i] + mat[i][mat.length - i - 1]
-        console.log(answer)
     }
     return n % 2 ? answer  - mat[Math.floor(n / 2)][Math.floor(n / 2)] : answer
 }
